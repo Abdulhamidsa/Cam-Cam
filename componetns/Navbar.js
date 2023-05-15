f7edf0e98cef9f977d926d8c71e303307d40bdb5
Aboood <abdu3793@stud.kea.dk>
2023-05-15 17:56:28 +0200

    adding data aproach to nav

--- componetns/Navbar.js
+++ componetns/Navbar.js
@@ -1,7 +1,71 @@
 ﻿"use client";
-
 import { useState } from "react";
 import styles from "../styles/Navbar.module.scss";
+
+const menuData = [
+  {
+    title: "Home",
+    url: "/",
+  },
+  {
+    title: "Services",
+    children: [
+      {
+        title: "Service 1",
+        children: [
+          {
+            title: "Sub-Service 1",
+            url: "/services/service1/subservice1",
+          },
+          {
+            title: "Sub-Service 2",
+            url: "/services/service1/subservice2",
+          },
+        ],
+      },
+      {
+        title: "Service 2",
+        url: "/services/service2",
+      },
+    ],
+  },
+  {
+    title: "Shop",
+    children: [
+      {
+        title: "Product 1",
+        children: [
+          {
+            title: "Sub Product 1",
+            url: "/shop/product1/subproduct1",
+          },
+          {
+            title: "Sub Product 2",
+            url: "/shop/product1/subproduct2",
+          },
+        ],
+      },
+      {
+        title: "Product 2",
+        url: "/shop/product2",
+        children: [
+          {
+            title: "Sub Product 3",
+            url: "/shop/product2/subproduct3",
+          },
+          {
+            title: "Sub Product 4",
+            url: "/shop/product2/subproduct4",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    title: "Contact",
+    url: "/contact",
+  },
+];
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -26,7 +90,7 @@ const Navbar = () => {
       setActiveSubMenu(null);
       setActiveSubSubMenu(null);
     }
-    setIsMenuOpen(true);
+    setIsMenuOpen(true); // Ensure that the menu is open when clicking on a menu item
   };
 
   const handleSubSubMenuClick = (subSubMenu) => {
@@ -47,73 +111,29 @@ const Navbar = () => {
     setActiveSubMenu(subMenu);
   };
 
-  const menuData = [
-    {
-      title: "Home",
-    },
-    {
-      title: "Services",
-      children: [
-        {
-          title: "Service 1",
-          children: [
-            {
-              title: "Sub-Service 1",
-            },
-            {
-              title: "Sub-Service 2",
-            },
-          ],
-        },
-        {
-          title: "Service 2",
-        },
-      ],
-    },
-    {
-      title: "Shop",
-      children: [
-        {
-          title: "Product 1",
-          children: [
-            {
-              title: "Sub Product 1",
-            },
-            {
-              title: "Sub Product 2",
-            },
-          ],
-        },
-        {
-          title: "Product 2",
-        },
-      ],
-    },
-    {
-      title: "Contact",
-    },
-  ];
+  const renderSubMenu = (subMenuItems) => {
+    return (
+      <ul className={`${styles.subMenu} ${isMenuOpen ? styles.slideIn : ""}`}>
+        {subMenuItems.map((item) => (
+          <li key={item.title} className={styles.subMenuItem} onClick={() => handleSubMenuClick(item.title)}>
+            <a href={item.url}>{item.title}</a>
+          </li>
+        ))}
+      </ul>
+    );
+  };
 
-  const renderSubMenu = (items) => (
-    <ul className={`${styles.subMenu} ${isMenuOpen ? styles.slideIn : ""}`}>
-      {items.map((item, index) => (
-        <li key={index} className={`${styles.subMenuItem} ${activeSubMenu === item.title ? styles.active : ""}`} onClick={() => handleSubMenuClick(item.title)}>
-          <span>{item.title}</span>
-          {item.children && activeSubMenu === item.title && renderSubMenu(item.children)}
-        </li>
-      ))}
-    </ul>
-  );
-
-  //   const renderSubSubMenu = (items) => (
-  //     <ul className={`${styles.subSubMenu} ${isMenuOpen ? styles.slideIn : ""}`}>
-  //       {items.map((item, index) => (
-  //         <li key={index} className={`${styles.subSubMenuItem} ${activeSubSubMenu === item.title ? styles.active : ""}`} onClick={() => handleSubSubMenuClick(item.title)}>
-  //           <span>{item.title}</span>
-  //         </li>
-  //       ))}
-  //     </ul>
-  //   );
+  const renderSubSubMenu = (subSubMenuItems) => {
+    return (
+      <ul className={`${styles.subSubMenu} ${isMenuOpen ? styles.slideIn : ""}`}>
+        {subSubMenuItems.map((item) => (
+          <li key={item.title} className={styles.subSubMenuItem} onClick={() => handleSubSubMenuClick(item.title)}>
+            <a href={item.url}>{item.title}</a>
+          </li>
+        ))}
+      </ul>
+    );
+  };
 
   return (
     <nav>
@@ -122,19 +142,28 @@ const Navbar = () => {
         <div className={`${styles.burgerBar} ${isMenuOpen ? styles.open : ""}`}></div>
         <div className={`${styles.burgerBar} ${isMenuOpen ? styles.open : ""}`}></div>
       </div>
-      <ul className={`${styles.navLinks} ${isMenuOpen ? styles.slideIn : ""}`}>
-        {menuData.map((item, index) => (
-          <li key={index} className={`${styles.navItem} ${activeMenu === item.title ? styles.active : ""}`}>
-            <span onClick={() => handleMenuClick(item.title)}>{item.title}</span>
-            {item.children && activeMenu === item.title && renderSubMenu(item.children)}
-          </li>
-        ))}
-      </ul>
       {activeMenu && (
         <div className={`${styles.backButton} ${isMenuOpen ? styles.slideIn : ""}`} onClick={handleBackClick}>
           &larr;
         </div>
       )}
+      <ul className={`${styles.navLinks} ${isMenuOpen ? styles.slideIn : ""}`}>
+        {menuData.map((menuItem) => (
+          <li key={menuItem.title} className={`${styles.navItem} ${activeMenu === menuItem.title ? styles.active : ""}`}>
+            {menuItem.children ? <span onClick={() => handleMenuClick(menuItem.title)}>{menuItem.title}</span> : <a href={menuItem.url}>{menuItem.title}</a>}
+          </li>
+        ))}
+      </ul>
+      {menuData.map((menuItem) => (
+        <li key={menuItem.title} className={`${styles.navItem} ${activeMenu === menuItem.title ? styles.active : ""}`}>
+          {activeMenu === menuItem.title && menuItem.children && (
+            <>
+              {renderSubMenu(menuItem.children)}
+              {activeSubMenu && menuItem.children.find((child) => child.title === activeSubMenu)?.children && renderSubSubMenu(menuItem.children.find((child) => child.title === activeSubMenu)?.children)}
+            </>
+          )}
+        </li>
+      ))}
     </nav>
   );
 };
